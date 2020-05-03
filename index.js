875edc75d87ed1fac2d3d19aae2e6a3134aa0b60
Anshuman Das <anshuman.das@gmail.com>
2020-05-03 08:28:02 +0530

    added logic to clearly show array differences

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const _ = require('lodash');
 const Chalk = require('chalk');
 
+function compare(a, b) {
+  var nameA = JSON.stringify(a);
+  var nameB = JSON.stringify(b);
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+}
+
 expect.extend({
   toBeSimilar(received, expected) {
     try {
@@ -8,7 +22,11 @@ expect.extend({
         expect(received).toBeNull();
       } else if(_.isArray(expected)) {
         expect(_.isArray(received)).toBe(true);
-        expect(received.length).toEqual(expected.length);
+        try{
+          expect(received.length).toEqual(expected.length);
+        } catch(e) {
+          expect(received.sort(compare)).toEqual(expected.sort(compare));
+        }
         expect(received).toBeSimilar(expect.arrayContaining(expected));
       } else if(_.isPlainObject(expected)) {
         expect(_.isPlainObject(received)).toBe(true);
